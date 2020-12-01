8f1b3a52c1333b39e2d6ee8d2fd2bf420b4dddc3
Smertin Nikita Job <smertin-nikita@mail.ru>
2020-12-01 14:00:44 +0500

    Shows most intelligence

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
+from pprint import pprint
+import requests
+
+TOKEN = '2619421814940190'
 
 
+def search_hero(name):
+    """Show heroes with name"""
+    response = requests.get(f'https://www.superheroapi.com/api.php/{TOKEN}/search/{name}')
+    response.raise_for_status()
 
-# Press the green button in the gutter to run the script.
+    data = response.json()
+    pprint(data)
+
+
+def get_hero(id):
+    """Returns hero by id"""
+    response = requests.get(f'https://www.superheroapi.com/api.php/{TOKEN}/{id}')
+    response.raise_for_status()
+
+    return response.json()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # search_hero('Thanos')
+    hulk = get_hero(332)
+    cap = get_hero(149)
+    thanos = get_hero(655)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    heroes = sorted([hulk, cap, thanos], key=lambda hero: hero['powerstats']['intelligence'])
+    for hero in heroes[:1]:
+        print(f"{hero['name']} intelligence: {hero['powerstats']['intelligence']}")
+
+
